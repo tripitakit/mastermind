614b21f9c4a37735c70f4631b92d272918a71822
tripitakit <patrick.demarta@gmail.com>
2018-05-08 18:52:26 +0200

    add distillery

--- board/mix.exs
+++ board/mix.exs
@@ -38,7 +38,8 @@ defmodule Board.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:mastermind, path: "../mastermind"}
+      {:mastermind, path: "../mastermind"},
+      {:distillery, "~> 1.5.2"}
     ]
   end
 end
